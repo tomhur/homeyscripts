99fa44f83c0fcd19c721ddcc490aa53c9fc43fef
Tommy Hurtig <tomhur@users.noreply.github.com>
2018-02-13 23:07:56 +0100

    create carheater.js

---
+++ carheater.js
@@ -0,0 +1,31 @@
+//turn on car heater based on outside temp so
+//the car is warm 30 min after alarm in the morning
+var hot = args[0];
+hot = hot.split(":");
+hot = Number(hot[0])*60 + Number(hot[1]) + 30;
+var now = new Date().getHours()*60 + new Date().getMinutes();
+
+if (now > hot) return false;
+
+var temp;
+let devices = await Homey.devices.getDevices();
+_.forEach(devices, device => {
+    if (device.name == "väder:Ute") {
+        temp = device.state.measure_temperature;
+    }
+});
+var before = Math.floor((temp-12)/-0.28);
+var toSleep = now - hot - before;
+/*
+console.log("Current time: " + now);
+console.log("Then the car should be hot: " + hot);
+console.log("Heater should start mins before: " + before);
+console.log("Mins to sleep before starting heater: " + toSleep);
+*/
+_.forEach(devices, device => {
+    if (device.name == "Garage outside") {
+        _.delay(function() {device.setCapabilityValue('onoff', true);} , toSleep*1000);
+    }
+});
+
+return true;
